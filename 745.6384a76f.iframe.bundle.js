e9a5ad91c393383ca0313a4a249c66d70655ba07
Frode Hansen <frode.hansen@bekk.no>
2023-02-22 17:10:20 +0100

    Updates

---
+++ 745.6384a76f.iframe.bundle.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_navikt_ds_datepicker=self.webpackChunk_navikt_ds_datepicker||[]).push([[745],{"./node_modules/react-dom/client.js":function(__unused_webpack_module,exports,__webpack_require__){var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
+//# sourceMappingURL=745.6384a76f.iframe.bundle.js.map
